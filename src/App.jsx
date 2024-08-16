e069ebfcd0707f6afa06031ba604de8f97357a23
Abdullah Al Maksud <maksud.workspace@gmail.com>
2024-08-16 17:03:21 +0600

    project setup with tailwind and material tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@material-tailwind/react';
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <Button className='bg-red-400'>Outlet</Button>
+    </div>
+  );
+};
+
+export default App;
