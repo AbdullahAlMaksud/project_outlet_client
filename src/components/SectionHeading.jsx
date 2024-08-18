2c867e6195cf051a3417cf9b0cb59449dd47084d
Abdullah Al Maksud <maksud.workspace@gmail.com>
2024-08-18 19:23:51 +0600

    fix some issue

--- src/components/SectionHeading.jsx
+++ src/components/SectionHeading.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 const SectionHeading = ({Heading}) => {
     return (
-        <div className="relative bg-cover bg-bottom lg:h-40 h-24 flex items-center justify-center rounded-bl-full overflow-clip">
-        <div className="absolute inset-0 bg-[url('./section-bg.svg')] bg-cover bg-bottom opacity-30">
-        </div>
+        <div className="bg-cover bg-bottom lg:h-40 h-24 flex items-center justify-center rounded-bl-full overflow-clip">
+        
         <h2 className='relative font-poppins text-3xl lg:text-4xl -rotate-2 drop-shadow-xl text-outlet-secondary font-bold'>
           {Heading}
         </h2>  
